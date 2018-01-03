262a86106c4f45cabb7ddc71164f6f54bbb5e27b
AngelPashov <angelpashov@AngelPashovs-Mac-mini.local>
2018-01-03 14:55:54 +0200

    ParallelStream exports

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const ObjectTransform = require('./src/ObjectTransform');
 const FilterTransform = require('./src/FilterTransform');
 const PassthroughStream = require('./src/PassthroughStream');
+const ParallelStream = require('./src/ParallelStream');
 
 module.exports = {
-    ObjectTransform,
-    FilterTransform,
-    PassthroughStream
+	ObjectTransform,
+	FilterTransform,
+	PassthroughStream,
+	ParallelStream,
 };
